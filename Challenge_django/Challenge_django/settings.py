8f2cb718e85ccb2ec0955687c7e924d2b7046902
Fidel <armelkouakou327@gmail.com>
2020-05-09 14:38:14 +0000

    Mise à jour du fichier en mettant les paramètres STATIC_ROOT,MEDIA_ROOT et MEDIA_URL nécessaires pour le lancement de la commande python manage.py collectstatic

--- Challenge_django/Challenge_django/settings.py
+++ Challenge_django/Challenge_django/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'Fr'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 
@@ -124,4 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
